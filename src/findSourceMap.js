b371144fa3445e461b6e980fd928699836fb20a7
tyler <tyler@chatbox.com>
2018-04-25 14:45:59 -0700

    fixed issue for webpack 4 fileobj structure

--- src/findSourceMap.js
+++ src/findSourceMap.js
@@ -1,9 +1,17 @@
 "use strict";
 module.exports = children => {
-    return children.reduce((m, i) => {
-        if (typeof i === "string" && i.includes("sourceMappingURL=")) {
-            m = i.split("sourceMappingURL=")[1];
-        }
-        return m;
+   return children.reduce((m, i) => {
+      let sourceMapPartials = [];
+
+       if(i._value) {
+          sourceMapPartials = i._value.split("sourceMappingURL=");
+       } else {
+          sourceMapPartials = i.split("sourceMappingURL=");
+       }
+
+       if (typeof i === "string" && sourceMapPartials.length > 1) {
+          m = i.split("sourceMappingURL=")[1];
+       }
+       return m;
     }, '');
 };
