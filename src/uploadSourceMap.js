df0be4d8770e704a0fb27954063ca7cebfe76cf5
tyler <tyler@chatbox.com>
2018-04-25 17:17:43 -0700

    reworked the lookup for generated sourcemaps

--- src/uploadSourceMap.js
+++ src/uploadSourceMap.js
@@ -1,44 +1,41 @@
 "use strict";
-const findSourceMap = require('./findSourceMap');
 const {publishSourcemap} = require('@newrelic/publish-sourcemap');
 
 module.exports = opts => item => {
-    const {
-        assets,
-        staticAssetUrlBuilder,
-        publicPath,
-        applicationId,
-        nrAdminKey,
-        url,
-        releaseName,
-        releaseId,
-        stats
-    } = opts;
+   const {
+      assets,
+      staticAssetUrlBuilder,
+      publicPath,
+      applicationId,
+      nrAdminKey,
+      url,
+      releaseName,
+      releaseId,
+      stats
+   } = opts;
 
-    const fileObj = assets[item];
-    if (!fileObj.children) {
-        return Promise.resolve();
-    }
+   const jsFileName = item.replace('.map', '');
 
-    const mapFile = assets[findSourceMap(fileObj.children)];
-    if (mapFile === undefined || !mapFile.emitted) {
-        return Promise.resolve();
-    }
+   const mapFile = assets[item];
+   if (mapFile === undefined || !mapFile.emitted) {
+      console.log('could not find source map', sourceMap);
+      return Promise.resolve();
+   }
 
-    const javascriptUrl = staticAssetUrlBuilder(url, publicPath, item, stats);
-    return new Promise((resolve, reject) => {
-        publishSourcemap({
-            sourcemapPath: mapFile.existsAt,
-            javascriptUrl,
-            applicationId,
-            nrAdminKey,
-            releaseName,
-            releaseId
-        }, (err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(javascriptUrl);
-        });
-    });
+   const javascriptUrl = staticAssetUrlBuilder(url, publicPath, jsFileName, stats);
+   return new Promise((resolve, reject) => {
+      publishSourcemap({
+                          sourcemapPath: mapFile.existsAt,
+      javascriptUrl,
+      applicationId,
+      nrAdminKey,
+      releaseName,
+      releaseId
+}, (err) => {
+      if (err) {
+         reject(err);
+      }
+      resolve(javascriptUrl);
+   });
+});
 };
